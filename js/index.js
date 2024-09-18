752556b5b4844c46375b6b126accd2e40a91ed6c
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2024-09-19 04:02:44 +0600

    added the input page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+console.log('aj rate khela hobe')
+document.getElementById('btn-login').addEventListener('click',function(event){
+    event.preventDefault();
+    const phnNumber= document.getElementById('phn-number').value;
+    
+    const pinNumber=document.getElementById('pin-number').value;
+    if(phnNumber === '56' && pinNumber==='1234'){
+        window.location.href='/home.html';
+    }else{
+        alert('please type right pin');
+    }
+})
