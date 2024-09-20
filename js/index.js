5ed94194c85c4a2d6f9a3a097f0499c696aebab1
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2024-09-20 16:41:10 +0600

    added the soliution

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.getElementById('btn-login').addEventListener('click',function(event){
     
     const pinNumber=document.getElementById('pin-number').value;
     if(phnNumber === '56' && pinNumber==='1234'){
-        window.location.href='/home.html';
+        window.location.href='./home.html';
     }else{
         alert('please type right pin');
     }
